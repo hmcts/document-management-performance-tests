8df9cb2d8019c735678fdae2ff69866cc6e24059
Alec Doran-Twyford <alec@alectronic.co>
2017-12-08 15:03:33 +0000

    switch to enviromental vars

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM maven:3.3-jdk-8
+FROM java:8-jdk
 
-RUN mkdir /tests
-COPY . /tests
+RUN mkdir -p tests
+COPY . tests
 
-ENTRYPOINT echo "EM_BASE_URI=$EM_BASE_URI\nIDAM_USER_BASE_URI=$IDAM_USER_BASE_URI\nIDAM_BASE_URI=$IDAM_BASE_URI\n" && wget --retry-connrefused --tries=120 --waitretry=1  -O /dev/null http://document-management-store:8080/health && cd /tests && mvn clean scala:testCompile gatling:execute -DdmApiGw=$EM_BASE_URI -DidamUser=$IDAM_USER_BASE_URI -DidamS2S=$IDAM_S2S_BASE_URI
+ENTRYPOINT echo "GRADLE VAR = $GRADLE_OPTS \n" && wget --retry-connrefused --tries=120 --waitretry=1 -O /dev/null http://document-management-store:8080/health && cd tests && ./gradlew $GRADLE_OPTS clean gatlingRun --info;
--- src/gatling/scala/uk/gov/hmcts/dm/pt/util/Environments.scala
+++ src/gatling/scala/uk/gov/hmcts/dm/pt/util/Environments.scala
@@ -2,15 +2,11 @@ package uk.gov.hmcts.dm.pt.util
 
 object Environments {
 
-  val defaultDmApiGw : String = "localhost:8080"
-  val defaultIdamUser : String = "localhost:8081"
-  val defaultIdamS2S : String = "localhost:8082"
-
-  val dmApiGw : String = System.getProperty("dmApiGw",defaultDmApiGw)
-  val idamUser : String = System.getProperty("idamUser",defaultIdamUser) + "/oauth2/authorize"
-  val idamS2S : String = System.getProperty("idamS2S",defaultIdamS2S) + "/testing-support/lease"
+  val dmApiGw : String = scala.util.Properties.envOrElse("DM_BASE_URI","localhost:8080")
+  val idamUser : String = scala.util.Properties.envOrElse("IDAM_USER_BASE_URI","localhost:8081") + "/oauth2/authorize"
+  val idamS2S : String = scala.util.Properties.envOrElse("IDAM_S2S_BASE_URI","localhost:8082") + "/testing-support/lease"
 
 
-  val users: String = scala.util.Properties.envOrElse("numberOfUsers", "500")
-  val maxResponseTime: String = scala.util.Properties.envOrElse("maxResponseTime", "3000000") // in milliseconds
+  val users: String = scala.util.Properties.envOrElse("NUMBER_OF_USERS", "500")
+  val maxResponseTime: String = scala.util.Properties.envOrElse("MAX_RES_TIME", "3000000") // in milliseconds
 }
